d786ff92becc62062ca9929c66af366129749b05
Josue Bryan Hernandez Zelaya <124007402+JBryan23@users.noreply.github.com>
2023-03-08 12:20:30 -0600

    Fin del programa

---
+++ EJERCICIOforJBHZ08032023/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("===============================");
+Console.WriteLine("Bienvenido usuario inteligente");
+Console.WriteLine("===============================");
+Console.WriteLine();
+
+Console.Write(@"Porfavor ingrese un numero entero que seria la altura 
+del triangulo a mostrar: ");
+
+int entero = int.Parse(Console.ReadLine());
+for (int y = 1; y<= entero; y++){
+    for(int x = 1; x<= y; x++){
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+Console.Write("Muchas gracias por utilizar el programa");
